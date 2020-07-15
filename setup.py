632405871ab133bb038de9b4e934f733e194abe0
Alister Cordiner <alister@canva.com>
2020-07-15 20:30:36 +1000

    Bump version to 0.2.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def get_requirements(filename):
 
 setup(
     name='scrapy-dynamodb',
-    version='0.2.1',
+    version='0.2.2',
     description='AWS DynamoDB pipeline for Scrapy',
     author='Alister Cordiner',
     author_email='alister@cordiner.net',
