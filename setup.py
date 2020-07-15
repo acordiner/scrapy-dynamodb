598c24281afa2cb31ded09f035416a86d04cfe07
Alister Cordiner <alister@canva.com>
2020-07-15 20:42:59 +1000

    Bump version to 0.2.3, set long description for PyPI

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 
 def get_requirements(filename):
@@ -11,10 +11,18 @@ def get_requirements(filename):
             if requirement and not requirement.startswith('#')
         ]
 
+
+def get_readme_contents():
+    with open('README.rst', encoding='utf-8') as f:
+        return f.read()
+
+
 setup(
     name='scrapy-dynamodb',
-    version='0.2.2',
+    version='0.2.3',
     description='AWS DynamoDB pipeline for Scrapy',
+    long_description=get_readme_contents(),
+    long_description_content_type='text/x-rst',
     author='Alister Cordiner',
     author_email='alister@cordiner.net',
     url='https://github.com/acordiner/scrapy-dynamodb',
